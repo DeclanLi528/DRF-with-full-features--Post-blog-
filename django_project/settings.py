86189b922684720120add0cb1d79f69ad6ece48b
Declan Li <DeclanLi528@gmail.com>
2025-08-09 17:41:43 +0800

    Blog API setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     #local
     "accounts",
     "posts",
+    # 3rd party apps
+    "rest_framework",
+    "corsheaders",
 
     "django.contrib.admin",
     "django.contrib.auth",
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -126,3 +130,16 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES":[
+        "rest_framework.permissions.AllowAny",
+    ]
+}
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from .serializers import Postserializer
+from rest_framework import generics
+from .models import Post
 # Create your views here.
+
+
+#ListCreateAPIView 支持GET和POST
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+
+#RetrieveUpdateDestroyAPIView 支持GET PUT/PATCH DELETE
+#完整的CRUD create read update delete
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
